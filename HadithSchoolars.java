18e5e80663becdfec89e241d6ec4794b74a2922f
kubi67 <144164774+kubi67@users.noreply.github.com>
2023-09-08 12:07:02 +0200

    Add files via upload

---
+++ HadithSchoolars.java
@@ -0,0 +1,20 @@
+
+	public class HadithSchoolars {
+		public static void main (String[] args) {
+			
+			System.out.println("Yahya ibn Maeen	:		- Kann 70000 Hadith auswendig ");
+			System.out.println("Ibn Hajar al Asqalani :	 	- Kann 65000 Hadith auswendig ");
+			System.out.println("Ishaq ibn Rahuyah :		- Kann 75000 Hadith auswendig ");
+			
+		{
+		int ibnHajar = 65000;
+		int yahyaibnmaeen = 70000;
+			
+		if(ibnHajar < yahyaibnmaeen) {
+		
+			System.out.println("Yahya ibn Maeen war ein stärkerer Hadithgelehrter als Ibn Hajar, jedoch war Ishaq stärker als Ibn Hajar und Yahya ibn Maeen ");
+	}
+		
+		}
+	}
+	}
